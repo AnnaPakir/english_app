41e62a949d9f79b5871fee7e03e018eef2912acb
Анна Матвеева <annamatveeva-adv@yandex.ru>
2025-07-02 22:22:32 +0500

    Add files via upload

---
+++ constants.ts
@@ -0,0 +1,15 @@
+import { CEFRLevel } from './types';
+
+export const CEFR_LEVELS_ORDER: CEFRLevel[] = [
+    CEFRLevel.A1,
+    CEFRLevel.A2,
+    CEFRLevel.B1,
+    CEFRLevel.B2,
+    CEFRLevel.C1,
+];
+
+// Progression System Constants
+export const PROGRESS_HISTORY_LENGTH = 100;
+export const PROGRESS_UNLOCK_THRESHOLD = 80;
+export const LEVEL_UP_TEST_QUESTIONS = 50;
+export const LEVEL_UP_PASS_PERCENTAGE = 0.8;
