cab85dff0d4bd4590ba241073226c73437191a28
Анна Матвеева <annamatveeva-adv@yandex.ru>
2025-07-19 13:24:52 +0500

    Delete components/common/Loader.tsx

--- components/common/Loader.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-interface LoaderProps {
-    text: string;
-}
-
-export const Loader: React.FC<LoaderProps> = ({ text }) => (<div className="flex flex-col items-center justify-center space-y-4"><div className="dot-flashing"></div><p className="text-gray-400">{text}</p></div>);
